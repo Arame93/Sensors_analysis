9d5c7363c4af5f9c6b4c8298dedfc3cd65a21ac0
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-05-05 13:18:48 +0000

    Update environmental_monitoring_app.py

--- environmental_monitoring_app.py
+++ environmental_monitoring_app.py
@@ -1,64 +1,16 @@
-#import streamlit as st
-#import pandas as pd
-#import plotly.express as px
-#import plotly.graph_objects as go
-#import json
-#import calendar
 import streamlit as st
-import geopandas as gpd
 import pandas as pd
-import leafmap.foliumap as leafmap
-from streamlit_folium import st_folium
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import calendar
+
 
 # Page setup
 st.set_page_config(layout="wide")
 st.title("Environmental Monitoring App")
 
-
-
-#st.set_page_config(layout="wide")
-#st.title("Choropleth Map with Leafmap")
-
-# Load GeoJSON (make sure region names or IDs match your data)
-geojson_path = "Sensors_data/map.geojson"
-gdf = gpd.read_file(geojson_path)
-
-# Load your environmental data (average per region)
-df = pd.read_csv("Sensors_data/air_quality_data.csv")
-
-# Replace value_type
-df['value_type'] = df['value_type'].replace(
-    ['P2', 'humidity', 'temperature', 'P1', 'pressure', 'durP1', 'durP2', 'P10'],
-    ['PM2.5', 'Humidity', 'Temperature', 'PM10', 'Pressure', 'durPM10', 'durPM2.5', 'PM10']
-)
-
-# Pivot data to get average by region and variable
-df["timestamp"] = pd.to_datetime(df["timestamp"], errors="coerce")
-df["month"] = df["timestamp"].dt.month
-df_avg = df.groupby(["region", "value_type"])["value"].mean().reset_index()
-df_pivot = df_avg.pivot(index="region", columns="value_type", values="value").reset_index()
-
-# Sidebar variable selector
-variable_options = df["value_type"].unique().tolist()
-selected_var = st.sidebar.selectbox("Select Variable", variable_options)
-
-# Merge with GeoDataFrame
-merged = gdf.merge(df_pivot[["region", selected_var]], left_on="region", right_on="region", how="left")
-
-# Plot
-m = leafmap.Map(center=[-1.3, 36.8], zoom=6)
-m.add_data(
-    merged,
-    column=selected_var,
-    legend_title=selected_var,
-    cmap="viridis",
-    info_mode="on_hover",
-    layer_name="Choropleth",
-)
-st_data = st_folium(m, width=1000, height=600)
-
-
-'''# Load data
+# Load data
 path = "Sensors_data/air_quality_data.csv"
 df = pd.read_csv(path)
 
@@ -143,4 +95,4 @@ if selected_vars:
     )
 
     # Plot the figure in Streamlit
-    st.plotly_chart(fig)'''
+    st.plotly_chart(fig)
