991a635b87799f9b537bc94222a9a9b2b9d8abe6
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-05-06 13:09:36 +0000

    Update environmental_monitoring_app.py

--- environmental_monitoring_app.py
+++ environmental_monitoring_app.py
@@ -4,41 +4,6 @@ import plotly.express as px
 import pydeck as pdk
 import calendar
 
-# Custom CSS to style the app layout
-st.markdown("""
-    <style>
-        /* Set the overall background color */
-        body {
-            background-color: #f0f2f6;
-        }
-        .main > div {
-            padding: 30px;
-        }
-
-        /* Container frame styling */
-        .app-container {
-            background-color: white;
-            padding: 40px;
-            border-radius: 12px;
-            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.05);
-            max-width: 1200px;
-            margin: 0 auto;
-        }
-
-        /* Title styling (optional) */
-        .app-title {
-            font-size: 32px;
-            font-weight: 700;
-            color: #333;
-            margin-bottom: 20px;
-            text-align: center;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-
-st.markdown("<div class='app-container'>", unsafe_allow_html=True)
-
 # Title and config
 st.set_page_config(layout="wide")
 #st.title("Environmental Monitoring App")
@@ -57,8 +22,6 @@ st.markdown("""
     </style>
     <div class="main-title">Environmental Monitoring App</div>
 """, unsafe_allow_html=True)
-
-st.markdown("</div>", unsafe_allow_html=True)
 
 # Load data
 df = pd.read_csv("Sensors_data/air_quality_data.csv")
